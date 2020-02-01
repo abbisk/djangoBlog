b7627996fb1af3a05842766afd9fb5737571c74d
abbisk <abbiskpro@gmail.com>
2020-02-01 20:50:06 +0530

    create tempelate

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,4 @@ urlpatterns=[
     path('index/', views.index, name= 'index'),
     path('get_blogs/', views.get_blogs, name='get blogs'),
     path('get_counts/', views.get_counts, name='get countsd'),
-]
+] 
