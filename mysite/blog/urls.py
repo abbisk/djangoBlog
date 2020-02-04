763095854596ea8a24537510c94d9c54346df450
abbisk <abbiskpro@gmail.com>
2020-02-04 23:48:49 +0530

    modify form and add edit option

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     path('index/', views.index, name= 'index'),
     path('get_blogs/', views.get_blogs, name='get blogs'),
     path('add_blog/', views.add_blog, name = 'add blog'),
+    path('add_blog/<int:blog_id>/', views.add_blog, name='edit_blog'),
 ] 
